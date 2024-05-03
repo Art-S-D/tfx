bacf468688ec58d076c83c51855d7483b3a20188
Art-S-D <darthopicoeur@gmail.com>
2024-05-03 12:11:33 +0200

    add json.ParseValue

---
+++ internal/json/json.go
@@ -0,0 +1,40 @@
+package json
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/Art-S-D/tfview/internal/render"
+)
+
+func ParseValue(jsonValue any, address string) render.Model {
+	switch value := jsonValue.(type) {
+	case string:
+		return &jsonString{value, address}
+	case float64:
+		return &jsonNumber{value, address}
+	case bool:
+		return &jsonBool{value, address}
+	case nil:
+		return &jsonNull{address}
+	case []any:
+		out := jsonArray{nil, address, false}
+		for i, v := range value {
+			addr := fmt.Sprintf("%s[%d]", address, i)
+			parsed := ParseValue(v, addr)
+			out.value = append(out.value, parsed)
+		}
+		return &out
+	case map[string]any:
+		out := jsonObject{make(map[string]render.Model), address, false}
+		for k, v := range value {
+			addr := fmt.Sprintf("%s.%s", address, k)
+			parsed := ParseValue(v, addr)
+			out.value[k] = parsed
+		}
+		return &out
+	default:
+		jsonType := reflect.TypeOf(jsonValue)
+		panic(fmt.Sprintf("unknown json value %v of type %v", jsonValue, jsonType))
+	}
+}
